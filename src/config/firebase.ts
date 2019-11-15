5da6944af9cbf951b66a1c09021b74691e10e438
Brian Emilius <brian@brianemilius.com>
2019-11-15 16:14:13 +0100

    rename data-storage to firebase

---
+++ src/config/firebase.ts
@@ -0,0 +1,11 @@
+import firebase from "firebase"
+
+export default firebase.initializeApp({
+	apiKey: process.env.apiKey,
+	authDomain: process.env.authDomain,
+	databaseURL: process.env.databaseURL,
+	projectId: process.env.projectId,
+	storageBucket: process.env.storageBucket,
+	messagingSenderId: process.env.messagingSenderId,
+	appId: process.env.appId
+})

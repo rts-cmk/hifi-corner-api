ba7849d1ef559591a1f633e5e5329c1aa65ab733
Brian Emilius <brian@brianemilius.com>
2019-11-18 21:23:02 +0100

    add category filter

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -3,7 +3,12 @@ import Product from "../models/products.model"
 
 export async function getAllProducts(req: e.Request, res: e.Response): Promise<void> {
 	try {
-		const response = await Product.get()
+		let response
+		if (req.query.category) {
+			response = await Product.where("category", "==", req.query.category).get()
+		} else {
+			response = await Product.get()
+		}
 		const result: Array<Record<string, any>> = []
 		response.forEach(item => result.push(item.data()))
 		res.json(result)

07d7ad962619ce7b90e487e8625e496cba4cdaa8
Brian Emilius <brian@brianemilius.com>
2019-11-19 13:00:09 +0100

    remove image route and controller

--- src/routes/images.route.ts
+++
@@ -1,6 +0,0 @@
-import e from "express"
-import { getAllImages } from "../controllers/images.controller"
-
-export default function route(router: e.Router): void {
-	router.get("/images", getAllImages)
-}

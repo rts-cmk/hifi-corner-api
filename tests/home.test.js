7cae739eb1980cacd2ce349117e579a76df9ace6
Brian Emilius <brian@brianemilius.com>
2019-11-20 09:03:55 +0100

    change test to match api docs page

--- tests/home.test.js
+++ tests/home.test.js
@@ -3,7 +3,7 @@ describe("The resource /", function() {
 		await page.goto("http://localhost:3000")
 	})
 
-	it("should have a welcome message", async function() {
-		await expect(await page.content()).toMatch("Hello, World!")
+	it("should have a title", async function() {
+		await expect(await page.content()).toMatch("HIFI Corner API")
 	})
 })
